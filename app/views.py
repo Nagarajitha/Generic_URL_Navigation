64b58e7b318ea9d5e7e7682d6361b3d2e2c8a718
Nagaranjitha Adhikari <nagaranjithareddy421@gmail.com>
2024-04-04 17:11:34 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def baskar(request):
+    return render(request,'baskar.html')
+
+def srujana(request):
+    return render(request,'srujana.html')
